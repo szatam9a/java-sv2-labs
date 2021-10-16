46fd97da4f80f55779086a48eb7163d47a2714ce
Szalay Tamás <szalaytamas90@gmail.com>
2021-10-16 18:39:26 +0200

    sinarlint segitségével megformázva

---
+++ src/main/java/conventions/Car.java
@@ -0,0 +1,40 @@
+package conventions;
+
+public class Car {
+
+    public void setCarType(String carType) {this.carType = carType;}
+    private String carType;
+    private String engineType;
+    private int persons;
+    private int doors;
+    public Car(String carType, String engineType, int doors, int persons) {
+        this.carType = carType;
+        this.engineType = engineType;
+        this.doors = doors;
+        this.persons = persons;
+    }
+    public int getPersons() {
+        return persons;
+    }
+    public String getCarType() {
+        return carType;
+    }
+    public String getEngineType() {
+        return engineType;
+    }
+    public int getDoors() {
+        return doors;
+    }
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+    public void setPersons(int persons) {
+        this.persons = persons;
+    }
+    public void addModelName(String modelName) {
+        this.carType = carType + " " + modelName;
+    }
+}
