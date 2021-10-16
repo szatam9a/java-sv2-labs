74e46782b42ca00135149b6add8507bb52f96a99
Szalay Tamás <szalaytamas90@gmail.com>
2021-10-16 14:31:30 +0200

    számológép kész 2 számot össze ad

---
+++ src/main/java/classstructureio/calculator.java
@@ -0,0 +1,23 @@
+package classstructureio;
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numbera,numberb;
+        numberb = 2;
+        numbera = 1;
+        System.out.println(numbera);
+        System.out.println(numberb);
+
+        System.out.println("whats the first number?");
+        numbera = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("whats the second number?");
+        numberb = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println(numbera + "+" + numberb);
+        System.out.println(numbera+numberb);
+
+    }
+}
