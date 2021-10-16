d531e4f666a13f7b440f9d20474fecef1c9849a0
Szalay Tamás <szalaytamas90@gmail.com>
2021-10-16 22:47:37 +0200

    dukomentáció olvasása

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,9 @@
+package javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+        String reverse = "Hello World";
+        reverse = reverse.toUpperCase();
+        System.out.println(reverse);
+    }
+}
