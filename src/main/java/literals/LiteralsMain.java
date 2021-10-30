89e55cb7b8f47420caef6d7fb70045424b964726
Szalay Tamás <szalaytamas90@gmail.com>
2021-10-30 23:24:16 +0200

    Literals

---
+++ src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,22 @@
+package literals;
+
+public class LiteralsMain {
+    public static void main(String[] args) {
+        String one = "1";
+        String two = "2";
+        String onePlusTwo = "1"+"2";
+        onePlusTwo= one+two;
+        System.out.println("1"+"2");
+        double quotient =3/4;
+        quotient = (double) 3/4;
+        //quotient = 3.0/4.0;
+        System.out.println(quotient);
+        long big = 3_244_444_444L;
+        System.out.println(big);
+        String word= "title";
+        System.out.println(word.toUpperCase());
+        String binary = Integer.toBinaryString(1);
+        System.out.println(Integer.toBinaryString(1));
+        System.out.println(Integer.toBinaryString(-2));
+    }
+}
