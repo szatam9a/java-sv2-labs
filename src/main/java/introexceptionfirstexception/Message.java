26327b35277514d39f2c1a8a17b16f296beed4ae
Szalay Tamás <szalaytamas90@gmail.com>
2021-11-20 13:18:23 +0100

    Funny!

---
+++ src/main/java/introexceptionfirstexception/Message.java
@@ -0,0 +1,22 @@
+package introexceptionfirstexception;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Message {
+    public static void main(String[] args) {
+        List<String> codedMessage = Arrays.asList("76", "101", "103", "121", "101", "110", "32", "115", "122", "233", "112", "32", "110", "97", "112", "111", "100", "33");
+        List<String> anotherCodedMessage = Arrays.asList("69", "122", "32", "101", "103", "121", "32", "104", "105", "98", "225", "115", "32", "252", "122", "101", "110", "101", "116", "46");
+
+        for (String actual:codedMessage
+             ) {
+            System.out.print((char) Integer.parseInt(actual));
+
+        }
+        for (String actual:anotherCodedMessage
+        ) {
+            System.out.print((char) Integer.parseInt(actual));
+
+        }
+    }
+}
