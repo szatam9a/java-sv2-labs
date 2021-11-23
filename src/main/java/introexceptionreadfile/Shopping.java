ca6243b2d7574b93fcc0d575b4693b637daa465a
Szalay Tamás <szalaytamas90@gmail.com>
2021-11-23 17:25:24 +0100

    readInFiles

--- src/main/java/introexceptionreadfile/Shopping.java
+++ src/main/java/introexceptionreadfile/Shopping.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.PrimitiveIterator;
 
 public class Shopping {
     private List<String>  items ;
