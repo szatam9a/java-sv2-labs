d01ced5cc48d653173f405b3ed073592d78c36f4
Szalay Tamás <szalaytamas90@gmail.com>
2021-11-24 17:06:57 +0100

    refactor methods

---
+++ src/main/java/introexceptioncause/TrackPoints.java
@@ -0,0 +1,37 @@
+package introexceptioncause;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrackPoints {
+    public List<Integer> heights = new ArrayList<>();
+
+    public void dataFiller() {
+        splitDataAndFillInToHeights(readIn(Path.of("src/main/resources/tracking.csv")));
+    }
+    public List<String> readIn(Path path) {
+        try {
+            return Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void splitDataAndFillInToHeights(List<String> list) {
+        String[] tmp;
+        for (String actual : list
+        ) {
+            tmp = actual.split(";");
+            heights.add(Integer.parseInt(tmp[2]));
+        }
+    }
+    public void calculateAndWriteOutTheWalkAltitude() {
+        for (int i = 1; i < heights.size(); i++) {
+            System.out.println(heights.get(i) - heights.get(i - 1));
+        }
+    }
+}
--- src/main/java/introexceptioncause/Tracking.java
+++ src/main/java/introexceptioncause/Tracking.java
@@ -1,49 +1,12 @@
 package introexceptioncause;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Tracking {
-    public List<Integer> heights = new ArrayList<>();
-
-    public Tracking() {
-        dataFiller();
-
-    }
-
-    public void dataFiller() {
-        splitData(readIn(Path.of("src/main/resources/tracking.csv")));
-    }
-
-    public List<String> readIn(Path path) {
-        try {
-            return Files.readAllLines(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public void splitData(List<String> list) {
-        String[] tmp;
-        for (String actual : list
-        ) {
-            tmp = actual.split(";");
-            heights.add(Integer.parseInt(tmp[2]));
-        }
-    }
-
-    public void calculateAndWriteOutTheWalkAltitude() {
-        for (int i = 1; i < heights.size(); i++) {
-            System.out.println(heights.get(i) - heights.get(i - 1));
-        }
-    }
-
     public static void main(String[] args) {
-        Tracking tracking = new Tracking();
-        tracking.calculateAndWriteOutTheWalkAltitude();
+        TrackPoints trackPoints = new TrackPoints();
+        trackPoints.dataFiller();
+        trackPoints.calculateAndWriteOutTheWalkAltitude();
+
     }
 }
