96a0bfd692bc318cc83319ea4bffa4f29c944a55
Szalay Tamás <szalaytamas90@gmail.com>
2022-01-23 21:10:08 +0100

    streams are great!I Love it!

--- src/main/java/streamsalgorithms/Employee.java
+++ src/main/java/streamsalgorithms/Employee.java
@@ -14,6 +14,10 @@ public class Employee {
         this.yearOfBirth = yearOfBirth;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public Employee() {
     }
 
@@ -26,21 +30,36 @@ public class Employee {
                 new Employee("Craft", 2000),
                 new Employee("Rontó Ralph", 1999)
         );
-
+        //Összegezd az alkalmazottak születési éveit!
         int sumBirthYears = list.stream().mapToInt(i -> i.yearOfBirth).sum();
         System.out.println(sumBirthYears);
+        //Összegezd az alkalmazottak életkorát!
         int sumAge = list.stream().mapToInt(i -> LocalDateTime.now().getYear() - i.yearOfBirth).sum();
         System.out.println(sumAge);
+        //Add vissza streammel, hány alkalmazott van a listában!
         System.out.println(list.stream().count());
+        //Add vissza, hány alkalmazott született 1990-nél korábban!
         System.out.println(list.stream().filter(e -> e.yearOfBirth < 1990).count());
+        //Add vissza a legkorábban született alkalmazott születési évét!
         OptionalInt firstGuy = list.stream().mapToInt(e -> e.yearOfBirth).min();
         System.out.println(list.stream().mapToInt(e -> e.yearOfBirth).min());
-        System.out.println(list.stream().filter(e -> e.yearOfBirth == firstGuy.getAsInt()).findFirst());
+        //Add vissza a legkorábban született alkalmazott nevét! (Itt szükség lesz a findFirst() záróműveletre.)
+
+        //System.out.println(list.stream().filter(e -> e.yearOfBirth == firstGuy.getAsInt()).findFirst());
+
+        System.out.println(list.stream().sorted((o1, o2) -> o1.yearOfBirth - o2.yearOfBirth).findFirst().map(e -> e.getName()));
+
+        //Add vissza, hogy igaz-e, hogy minden alkalmazott 1980 előtt született-e?
         System.out.println(list.stream().allMatch(e -> e.yearOfBirth < 1980));
-        List<Employee> before1990 = list.stream().filter(e -> e.yearOfBirth < 1990).collect(Collectors.toList());
+        //Add vissza egy listában az összes, 1990-nél korábban született alkalmazottat!
+        List<Employee> before1990 = list.stream()
+                .filter(e -> e.yearOfBirth < 1990)
+                .collect(Collectors.toList());
         System.out.println(before1990);
+        //Add vissza egy listában az összes alkalmazott nevét!
         List<String> names = list.stream().map(e -> e.name).collect(Collectors.toList());
         System.out.println(names);
+        //Az előző két feladat kombinációja: Add vissza egy listában az összes, 1990-nél korábban született alkalmazott nevét!
         System.out.println(list.stream().filter(e -> e.yearOfBirth < 1990).map(e -> e.name).collect(Collectors.toList()));
 
     }
